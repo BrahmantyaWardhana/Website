3be5ea4b2c6b7f23aaf5404bd7b6f6cbf8cea4fa
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-04-10 13:21:36 -0500

    improved sensor menus

--- sensor12.php
+++ sensor12.php
@@ -14,15 +14,31 @@
         </ul>
       </nav>
     </header>
-    <div class="sensor-image-container">
-      <h1>Sensor 12</h1>
-      <div class="image12">
-
+    <div class="grid-container">
+      <div class="header">
+        <h2>Sensor 12</h2>
       </div>
-    </div>
-    <div class="button-container">
-      <button class="button">Increase by x</button>
-      <button class="button">Lower by x</button>
-    </div>
-    <script src="js/script.js"></script>
+  
+      <div class="left">Inputs        
+        <form>
+          <input type="text" placeholder="Distance" />
+          <button>Enter</button>
+        </form>
+        <form>
+          <input type="text" placeholder="Power" />
+          <button>Enter</button>
+        </form>
+        <button class="button">Get Image</button>
+      </div>
+      <div class="middle">Image</div>  
+      <div class="right">
+        <p id="temperature1">Temperature</p>
+        <p id="battery1">Battery</p>
+        <p id="cameraStatus1">Initial Text</p>
+        <div class="button" id="button1" onclick="sendMessage('Refresh 1')">
+          <p>Button</p>
+        </div>
+      </div>
+
+    </div>  
   </body>
